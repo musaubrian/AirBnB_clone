508985a5a17afca4f134f960227914a90e58de1e
musaubrian <musaubrian15@gmail.com>
2022-08-03 16:58:21 +0300

    add file storage file

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""
+initialize package
+"""
+
+from engine.file_storage import FileStorage
---
+++ models/engine/file_storage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""file stroage module"""
+
+import json
+
+
+class FileStorage:
+    """
+    defines a file storage class
+    """
