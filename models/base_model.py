95de6793184b4ebcc6008d46243b1b4b08ee97fe
musaubrian <musaubrian15@gmail.com>
2022-08-03 16:34:49 +0300

    update base_model
    add str representation

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,40 @@
 #!/usr/bin/env python3
 """base class model to be inherited by other classes"""
 
+from uuid import uuid4
+from datetime import datetime
+import json
+
 
 class BaseModel:
     """
     defines base model class
     """
+
+    def __init__(self, *args, **kwargs) -> None:
+        """instatiate base_class"""
+
+        if len(kwargs) == 0:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            kwargs["created_at"] = datetime.strptime(
+                    kwargs["created_at"],
+                    "%Y-%m-%dT%H:%M:%S.%f"
+                    )
+            kwargs["updated_at"] = datetime.strptime(
+                    kwargs["updated_at"],
+                    "%Y-%m-%dT%H:%M:%S.%f"
+                    )
+            for k, v in kwargs.items():
+                if "__class__" not in k:
+                    setattr(self, k, v)
+
+    def __str__(self):
+        """
+        return string representation of the class
+        """
+        return (
+                f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+                )
