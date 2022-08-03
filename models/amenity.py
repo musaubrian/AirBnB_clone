0ac96801792f3b7ab80e193a4484af1646a15c24
musaubrian <musaubrian15@gmail.com>
2022-08-03 15:50:36 +0300

    fill 'inheritors' classes

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""user module for the Airbnb project"""
+from base_model import BaseModel as base
+
+
+class Amenity(base):
+    """defines amenity class inheriting class base"""
+    name = " "
---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""module for the city"""
+
+from base_model import BaseModel as base
+
+
+class City(base):
+    """city class containing state id and name"""
+    name = ""
+    state_id = ""
