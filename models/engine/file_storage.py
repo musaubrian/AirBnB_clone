1956ceecf738884d3034b9f41daeb3c199e6ebc7
musaubrian <musaubrian15@gmail.com>
2022-08-03 17:28:28 +0300

    add new and save methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,3 +8,34 @@ class FileStorage:
     """
     defines a file storage class
     """
+
+    __path = "file_storage.json"
+    __objects = {}
+
+    def all(self):
+        """
+        return all __objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        create new __object instance
+        args:
+            obj: instance object
+        """
+        
+        k = f"{obj.__class__.__name__}.{obj.id}"
+        v = obj
+        FileStorage.__objects[k] = v
+
+    def save(self):
+        """
+        saves to json file
+        """
+        objects_dict = {}
+        for k, v in FileStorage.__objects.items():
+            objects_dict[k] = v.to_dict()
+
+        with open(FileStorage.__path, "w") as file:
+            json.dump(objects_dict, file)
