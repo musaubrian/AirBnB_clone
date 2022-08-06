11addc3da090db42a533378ec310ae0fc4ad8d7e
musaubrian <musaubrian15@gmail.com>
2022-08-06 10:17:32 +0300

    import modules correctly

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,21 @@ initialize package
 """
 
 from models.engine.file_storage import FileStorage
+from models.amenity import Amenity
+from models.base_model import BaseModel as base
+from models.city import City
+from models.place import Place
+from models.review import Reviews
+from models.state import State
+from models.user import User
 
-storage = FileStorage
+
+my_classes = {
+        "Amenity": Amenity, "BaseModel": base,
+        "City": City, "Place": Place,
+        "Reviews": Reviews, "State": State,
+        "User": User
+        }
+
+storage = FileStorage()
 storage.reload()
